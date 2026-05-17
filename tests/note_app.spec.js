2bca66fcafe798510a839f7f4581bced3067f677
Nuul <nuul@example.com>
2026-05-17 13:25:58 -0600

    Init

---
+++ tests/note_app.spec.js
@@ -0,0 +1,47 @@
+const { test, expect, describe, beforeEach } = require('@playwright/test')
+
+describe('Note app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Admin',
+        username: 'admin',
+        password: 'passw'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+  test('front page can be opened', async ({ page }) => {
+
+    const locator = page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', {name: 'log in'}).click()
+    await page.getByRole('textbox', { name: 'username' }).fill('admin')
+    await page.getByRole('textbox', { name: 'password' }).fill('passw')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('logged in as: Admin')).toBeVisible()
+  })
+
+  describe('when logged in', ()=> {
+    beforeEach(async ({page}) => {
+      await page.getByRole('button', { name:'log in' }).click()
+      await page.getByLabel('username').fill('admin')
+      await page.getByLabel('password').fill('passw')
+      await page.getByRole('button', { name: 'login'}).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+  })
+})
