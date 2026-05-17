adc98235660eae55e3700e93ebba89c75b630c2e
Nuul <nuul@example.com>
2026-05-17 14:04:08 -0600

    Add 'importance can be changed' test

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -43,5 +43,18 @@ describe('Note app', () => {
       await page.getByRole('button', { name: 'save' }).click()
       await expect(page.getByText('a note created by playwright')).toBeVisible()
     })
+
+    describe('and a note exists', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note'}).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
+    })
   })
 })
