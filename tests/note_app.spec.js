bc85397f2988cd617bf8a89f7b0c0b0ff1657066
Nuul <nuul@example.com>
2026-05-19 16:54:30 -0600

    Replace 'importance can be changed' test for 'one of those can be made nonimportant', and Fix createNote helper function to wait for content

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -51,13 +51,24 @@ describe('Note app', () => {
 
     describe('and a note exists', () => {
       beforeEach(async ({ page }) => {
-        await createNote(page, 'another note by playwright')
+        await createNote(page, 'first note')
+        await createNote(page, 'second note')
+        await createNote(page, 'third note')
       })
 
-      test('importance can be changed', async ({ page }) => {
-        await page.getByRole('button', { name: 'make not important' }).click()
-        await expect(page.getByText('make important')).toBeVisible()
-      })
+      // test('importance can be changed', async ({ page }) => {
+      //   await page.getByRole('button', { name: 'make not important' }).click()
+      //   await expect(page.getByText('make important')).toBeVisible()
+      // })
+
+      test('one of those can be made nonimportant', async ({ page }) => {
+        page.pause()
+        const otherNoteText = page.getByText('second note')      
+        const otherNoteElement = otherNoteText.locator('..')
+      
+        await otherNoteElement.getByRole('button', { name: 'make not important' }).click()
+        await expect(otherNoteElement.getByText('make important')).toBeVisible()
+    })
     })
   })
 })
